f2377e4d9ff0c921713fd5d4c285aa100792edb1
rabbi mathake <mathakerabbi@hotmail.com>
2022-03-17 23:00:14 +0200

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("81a3be8a-4731-4686-b590-48573fb74750")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Text;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //sort text lines
+        [TestMethod]
+        public void sortTextLines()
+        {
+            //filename
+            string filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "dws.txt");
+
+            if (File.Exists(filename))
+            {
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(filename);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+
+                // Set a variable to the Documents path.
+                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // Write sorted lines to new file
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "DWSFileSort.txt")))
+                {
+                    //sort lines
+                    Array.Sort(lines);
+
+                    //write to new file
+                    foreach (string line in lines)
+                    {
+                        outputFile.WriteLine(line);
+                    }
+                }
+            }
+        }
+
+        //reverse the order of text file lines
+        [TestMethod]
+        public  void reverseOrderLines()
+        {
+            //filename
+            string filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "dws.txt");
+
+            if (File.Exists(filename))
+            {
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(filename);
+
+                // Set a variable to the Documents path.
+                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // Write sorted lines to new file
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "DWSFileReverseSort.txt")))
+                {
+                    //sort lines
+                    Array.Reverse(lines);
+
+                    //write to new file
+                    foreach (string line in lines)
+                    {
+                        outputFile.WriteLine(line);
+                    }
+                }
+            }
+        }
+
+        //Move each char 20 places
+        [TestMethod]
+        public  void moveChar()
+        {
+            //filename
+            string filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "dws.txt");
+
+            if (File.Exists(filename))
+            {
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(filename);
+
+                // Set a variable to the Documents path.
+                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // Write sorted lines to new file
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "DWSFileASCII.txt")))
+                {
+                    //write to new file
+                    foreach (string line in lines)
+                    {
+                        outputFile.WriteLine(Encoding.ASCII.GetBytes(line));
+                    }
+                }
+            }
+        }
+
+        //Calculate the length of the text
+        [TestMethod]
+        public  void calculateLength()
+        {
+            //filename
+            string filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "dws.txt");
+
+            if (File.Exists(filename))
+            {
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(filename);
+
+                // Set a variable to the Documents path.
+                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // Write sorted lines to new file
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "DWSFileLineLength.txt")))
+                {
+                    //write to new file
+                    foreach (string line in lines)
+                    {
+                        outputFile.WriteLine(line.Length + " " + line);
+                    }
+                }
+            }
+        }
+    }
+}
