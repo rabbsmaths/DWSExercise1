36692c02466b0fd30ca4a8da7e92c7db63b54af0
rabbi mathake <mathakerabbi@hotmail.com>
2022-03-17 22:55:03 +0200

    methods added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
+using System.Text;
 
 namespace Exercise1
 {
@@ -9,39 +11,49 @@ namespace Exercise1
         static void Main(string[] args)
         {
             //filename
-            string readerStream = @"C:\dws.txt";
-            //FileStream readerStream = new FileStream(@"C:\dws.txt", FileMode.Open);
-            sortTextLines(readerStream);
+            string filepath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),"dws.txt");
+
+            //readfile and sort
+            sortTextLines(filepath);
+
+            //readfile and reverse sort
+            reverseOrderLines(filepath);
+
+            //calculate the text line length
+            calculateLength(filepath);
+
+            //Encrypt
+            moveChar(filepath);
         }
 
         //sort text lines
         public static void sortTextLines(string filename)
         {
-            string[] content = null;
-            //read file
-            using (StreamReader file = new StreamReader(filename))
+            if (File.Exists(filename))
             {
-                int counter = 0;
-                string ln;
-
-                while ((ln = file.ReadLine()) != null)
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(filename);
+                foreach (string line in lines)
                 {
-                    content = file.ReadToEnd().Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-
-                    Console.WriteLine(ln);
-                    counter++;
+                    Console.WriteLine(line);
                 }
-                file.Close();
-                Console.WriteLine($"File has {counter} lines.");
-            }
 
-            //sort
-            filename = @"C:\DWSFileSort.txt";
-            using (StreamWriter writer = new StreamWriter(filename))
-            {
-                //Sort the content and write new file
-                Array.Sort(content);
-                writer.Write(string.Join(Environment.NewLine, content));
+
+                // Set a variable to the Documents path.
+                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // Write sorted lines to new file
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "DWSFileSort.txt")))
+                {
+                    //sort lines
+                    Array.Sort(lines);
+
+                    //write to new file
+                    foreach (string line in lines)
+                    {
+                        outputFile.WriteLine(line);
+                    }
+                }
             }
         }
 
@@ -49,18 +61,73 @@ namespace Exercise1
         public static void reverseOrderLines(string filename)
         {
 
+            if (File.Exists(filename))
+            {
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(filename);
+
+                // Set a variable to the Documents path.
+                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // Write sorted lines to new file
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "DWSFileReverseSort.txt")))
+                {
+                    //sort lines
+                    Array.Reverse(lines);
+
+                    //write to new file
+                    foreach (string line in lines)
+                    {
+                        outputFile.WriteLine(line);
+                    }
+                }
+            }
         }
 
         //Move each char 20 places
         public static void moveChar(string filename)
         {
+            if (File.Exists(filename))
+            {
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(filename);
 
+                // Set a variable to the Documents path.
+                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // Write sorted lines to new file
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "DWSFileASCII.txt")))
+                {
+                    //write to new file
+                    foreach (string line in lines)
+                    {
+                        outputFile.WriteLine(Encoding.ASCII.GetBytes(line));
+                    }
+                }
+            }
         }
 
         //Calculate the length of the text
         public static void calculateLength(string filename)
         {
+            if (File.Exists(filename))
+            {
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(filename);
 
+                // Set a variable to the Documents path.
+                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // Write sorted lines to new file
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "DWSFileLineLength.txt")))
+                {
+                    //write to new file
+                    foreach (string line in lines)
+                    {
+                        outputFile.WriteLine(line.Length+" "+line);
+                    }
+                }
+            }
         }
     }
 }
